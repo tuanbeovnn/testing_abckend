a4b18c22f2dac6a605094f084174648ed8189bfd
Tuan Nguyen <tuan@pezzie.com>
2023-03-30 22:32:51 +0300

    Remove unimported.

--- src/main/java/com/myblogbackend/blog/config/FlywayConfig.java
+++ src/main/java/com/myblogbackend/blog/config/FlywayConfig.java
@@ -6,8 +6,6 @@ import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
