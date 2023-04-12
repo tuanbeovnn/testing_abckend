2827086d96f9891d473f9a20ea4a87075d263454
Tuan Nguyen <tuan.nguyen@svea.fi>
2023-04-12 13:44:56 +0300

    Updated profile testing.

--- src/test/java/com/myblogbackend/blog/category/CategoryApiDelegateImplTest.java
+++ src/test/java/com/myblogbackend/blog/category/CategoryApiDelegateImplTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,6 +31,7 @@ import static org.mockito.ArgumentMatchers.any;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class CategoryApiDelegateImplTest {
     @Autowired
     private MockMvc mockMvc;
